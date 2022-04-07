ab6e8011d7cdb9d17ce226db92e2a94f4d9bc53b
Karolishp <karolis.spukas@gmail.com>
2022-04-07 15:15:50 +0100

    #7 Add SGC benchmarks

--- benchmarks/benchmark_semi_supervised.py
+++ benchmarks/benchmark_semi_supervised.py
@@ -3,6 +3,7 @@ import torch
 from catgnn.datasets.planetoid import PlanetoidDataset
 import torch_geometric
 from benchmarks.models.semi_supervised_models.gcn_models import GCN_1_Paper, GCN_2_Paper, PyG_GCN_Paper
+from benchmarks.models.semi_supervised_models.sgc_models import SGC_2_Paper, PyG_SGC_Paper
 import numpy as np
 from benchmarks.utils.analyse_performance import analyse_repeated_benchmark, stringify_statistics
 
@@ -45,24 +46,25 @@ def repeat_benchmark(repeat, func, experiment_name, *args, **kwargs):
     return analyse_repeated_benchmark(train_accs, val_accs, test_accs, avg_runtimes, repeat)
 
 
-def run_all_benchmarks(repeat=5):
-    # Cora benchmarks for paper reproducability
+def run_paper_benchmarks(name='Cora', repeat=2):
+    # Benchmarks for paper reproducability on both full and random splits
     """
     experiment_0 = 'Cora CatGNN GCN (1)'
     train_res_0, val_res_0, test_res_0, runtime_res_0 = repeat_benchmark(repeat, run_benchmark, 'Cora', GCN_1_Paper)
     """
 
-    experiment_1 = 'Cora CatGNN GCN (2)'
-    train_res_1, val_res_1, test_res_1, runtime_res_1 = repeat_benchmark(repeat, run_benchmark, experiment_1, 'Cora', GCN_2_Paper)
+    """
+    experiment_1 = 'Cora CatGNN GCN (2), public split'
+    train_res_1, val_res_1, test_res_1, runtime_res_1 = repeat_benchmark(repeat, run_benchmark, experiment_1, name, GCN_2_Paper)
 
-    experiment_2 = 'Cora CatGNN GCN (2, factored)'
-    train_res_2, val_res_2, test_res_2, runtime_res_2 = repeat_benchmark(repeat, run_benchmark, experiment_2, 'Cora', GCN_2_Paper, factored=True)
+    experiment_2 = 'Cora CatGNN GCN (2, factored), public split'
+    train_res_2, val_res_2, test_res_2, runtime_res_2 = repeat_benchmark(repeat, run_benchmark, experiment_2, name, GCN_2_Paper, factored=True)
 
-    experiment_3 = 'Cora CatGNN GCN (2, factored)'
-    train_res_3, val_res_3, test_res_3, runtime_res_3 = repeat_benchmark(repeat, run_benchmark, experiment_3, 'Cora', GCN_2_Paper, forwards=True)
+    experiment_3 = 'Cora CatGNN GCN (2, forwards), public split'
+    train_res_3, val_res_3, test_res_3, runtime_res_3 = repeat_benchmark(repeat, run_benchmark, experiment_3, name, GCN_2_Paper, forwards=True)
 
-    experiment_4 = 'Cora PyG GCN'
-    train_res_4, val_res_4, test_res_4, runtime_res_4 = repeat_benchmark(repeat, run_benchmark, experiment_4, 'Cora', PyG_GCN_Paper)
+    experiment_4 = 'Cora PyG GCN, public split'
+    train_res_4, val_res_4, test_res_4, runtime_res_4 = repeat_benchmark(repeat, run_benchmark, experiment_4, name, PyG_GCN_Paper)
 
     print('')
     print(stringify_statistics(experiment_1, train_res_1, val_res_1, test_res_1, runtime_res_1))
@@ -70,15 +72,37 @@ def run_all_benchmarks(repeat=5):
     print(stringify_statistics(experiment_3, train_res_3, val_res_3, test_res_3, runtime_res_3))
     print(stringify_statistics(experiment_4, train_res_4, val_res_4, test_res_4, runtime_res_4))
 
-    # CiteSeer benchmarks for a single layer
-    # TODO
+    experiment_5 = 'Cora CatGNN GCN (2), random split'
+    train_res_5, val_res_5, test_res_5, runtime_res_5 = repeat_benchmark(repeat, run_benchmark, experiment_5, name, GCN_2_Paper, split='random')
 
-    # PubMed benchmarks for a single layer
-    # TODO
+    experiment_6 = 'Cora CatGNN GCN (2, factored), random split'
+    train_res_6, val_res_6, test_res_6, runtime_res_6 = repeat_benchmark(repeat, run_benchmark, experiment_6, name, GCN_2_Paper, split='random', factored=True)
 
-    # Cora benchmarks for how it scales with several layers
-    # TODO
+    experiment_7 = 'Cora CatGNN GCN (2, forwards), random split'
+    train_res_7, val_res_7, test_res_7, runtime_res_7 = repeat_benchmark(repeat, run_benchmark, experiment_7, name, GCN_2_Paper, split='random', forwards=True)
+
+    experiment_8 = 'Cora PyG GCN, random split'
+    train_res_8, val_res_8, test_res_8, runtime_res_8 = repeat_benchmark(repeat, run_benchmark, experiment_8, name, PyG_GCN_Paper, split='random')
+
+    print('')
+    print(stringify_statistics(experiment_5, train_res_5, val_res_5, test_res_5, runtime_res_5))
+    print(stringify_statistics(experiment_6, train_res_6, val_res_6, test_res_6, runtime_res_6))
+    print(stringify_statistics(experiment_7, train_res_7, val_res_7, test_res_7, runtime_res_7))
+    print(stringify_statistics(experiment_8, train_res_8, val_res_8, test_res_8, runtime_res_8))
+    """
+
+    experiment_9 = 'Cora CatGNN SGC (2), public split'
+    train_res_9, val_res_9, test_res_9, runtime_res_9 = repeat_benchmark(repeat, run_benchmark, experiment_9, name, SGC_2_Paper, K=3)
+
+    experiment_10 = 'Cora PyG SGC, public split'
+    train_res_10, val_res_10, test_res_10, runtime_res_10 = repeat_benchmark(repeat, run_benchmark, experiment_10, name, PyG_SGC_Paper, K=3)
+
+    print('')
+    print(stringify_statistics(experiment_9, train_res_9, val_res_9, test_res_9, runtime_res_9))
+    print(stringify_statistics(experiment_10, train_res_10, val_res_10, test_res_10, runtime_res_10))
 
 
 if __name__ == '__main__':
-    run_all_benchmarks()
+    run_paper_benchmarks() # Cora
+    # run_paper_benchmarks(name='CiteSeer')
+    # run_paper_benchmarks(name='PubMed')
--- benchmarks/models/semi_supervised_models/sgc_models.py
+++ benchmarks/models/semi_supervised_models/sgc_models.py
@@ -8,6 +8,11 @@ from catgnn.datasets.planetoid import PlanetoidDataset
 import torch_geometric
 
 
+"""
+General layers
+"""
+
+
 class SGC_2(nn.Module):
 
     def __init__(self, input_dim, output_dim):
@@ -26,3 +31,28 @@ class PyG_SGC(nn.Module):
 
     def forward(self, V, E, X):
         return self.gcn_layer(X, E)
+
+
+"""
+Paper benchmarking
+"""
+
+
+class SGC_2_Paper(torch.nn.Module):
+    def __init__(self, input_dim, output_dim, K=3):
+        super().__init__()
+        self.conv1 = SGCLayer_MPNN_2(input_dim, output_dim, K=K)
+
+    def forward(self, V, E, X):
+        H = self.conv1(V, E, X)
+        return H
+
+
+class PyG_SGC_Paper(torch.nn.Module):
+    def __init__(self, input_dim, output_dim, K=3):
+        super().__init__()
+        self.conv1 = torch_geometric.nn.conv.SGConv(input_dim, output_dim, K=K, cached=False, add_self_loops=True)
+
+    def forward(self, V, E, X):
+        H = self.conv1(X, E)
+        return H
