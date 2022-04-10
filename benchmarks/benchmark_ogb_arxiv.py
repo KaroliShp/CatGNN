aea71fac39ca67690b1d330248eec052a8ba5e3f
Karolishp <karolis.spukas@gmail.com>
2022-04-10 20:39:51 +0100

    #11 Add OGB benchmark code

--- benchmarks/benchmark_ogb_arxiv.py
+++ benchmarks/benchmark_ogb_arxiv.py
@@ -6,7 +6,11 @@ This benchmark code from:
 https://github.com/snap-stanford/ogb/blob/master/examples/nodeproppred/arxiv/gnn.py
 """
 
+from ogb.nodeproppred import PygNodePropPredDataset, Evaluator # Sometimes buggy? Something wrong with acquiring lock in OGB
+
 import argparse
+from timeit import default_timer as timer
+from datetime import timedelta
 
 import torch
 import torch.nn.functional as F
@@ -15,8 +19,6 @@ import torch_geometric.transforms as T
 from torch_geometric.nn import GCNConv, SAGEConv
 from catgnn.layers.gcn.gcn_mpnn_2 import GCNLayer_MPNN_2
 from catgnn.layers.sage.sage_mpnn_2 import SAGELayer_MPNN_2
-
-from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
 
 
 """
@@ -55,6 +57,7 @@ class GCN(torch.nn.Module):
             x = F.relu(x)
             x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.convs[-1](V, adj_t, x)
+        return x.log_softmax(dim=-1)
 
 
 class PyG_GCN(torch.nn.Module):
@@ -208,11 +211,14 @@ class Logger(object):
             print(f'   Final Test: {r.mean():.2f} ± {r.std():.2f}')
 
 
-def train(model, data, train_idx, optimizer):
+def train(model, data, train_idx, optimizer, is_sparse):
     model.train()
 
     optimizer.zero_grad()
-    out = model(data.x, data.adj_t)[train_idx]
+    if is_sparse:
+        out = model(data.x, data.adj_t)[train_idx]
+    else:
+        out = model(data.x, data.edge_index)[train_idx]
     loss = F.nll_loss(out, data.y.squeeze(1)[train_idx])
     loss.backward()
     optimizer.step()
@@ -221,10 +227,13 @@ def train(model, data, train_idx, optimizer):
 
 
 @torch.no_grad()
-def test(model, data, split_idx, evaluator):
+def test(model, data, split_idx, evaluator, is_sparse):
     model.eval()
 
-    out = model(data.x, data.adj_t)
+    if is_sparse:
+        out = model(data.x, data.adj_t)
+    else:
+        out = model(data.x, data.edge_index)
     y_pred = out.argmax(dim=-1, keepdim=True)
 
     train_acc = evaluator.eval({
@@ -244,7 +253,6 @@ def test(model, data, split_idx, evaluator):
 
 
 def main():
-    print('Main')
     parser = argparse.ArgumentParser(description='OGBN-Arxiv (GNN)')
     parser.add_argument('--device', type=int, default=0)
     parser.add_argument('--log_steps', type=int, default=1)
@@ -255,31 +263,48 @@ def main():
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--epochs', type=int, default=500)
     parser.add_argument('--runs', type=int, default=10)
+    parser.add_argument('--catgnn', type=bool, default=False)
     args = parser.parse_args()
     print(args)
-    print('Main')
 
     device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
     device = torch.device(device)
 
-    dataset = PygNodePropPredDataset(name='ogbn-arxiv',
-                                     transform=T.ToSparseTensor())
-
-    data = dataset[0]
-    data.adj_t = data.adj_t.to_symmetric()
+    if args.catgnn:
+        dataset = PygNodePropPredDataset(name='ogbn-arxiv')
+        data = dataset[0]
+    else:
+        dataset = PygNodePropPredDataset(name='ogbn-arxiv',
+                                        transform=T.ToSparseTensor())
+        data = dataset[0]
+        data.adj_t = data.adj_t.to_symmetric()
     data = data.to(device)
 
     split_idx = dataset.get_idx_split()
     train_idx = split_idx['train'].to(device)
 
     if args.use_sage:
-        model = PyG_SAGE(data.num_features, args.hidden_channels,
-                     dataset.num_classes, args.num_layers,
-                     args.dropout).to(device)
+        if args.catgnn:
+            print('--CATGNN SAGE--')
+            model = SAGE(data.num_features, args.hidden_channels,
+                         dataset.num_classes, args.num_layers, 
+                         args.dropout).to(device)
+        else:
+            print('--PyG SAGE--')
+            model = PyG_SAGE(data.num_features, args.hidden_channels,
+                             dataset.num_classes, args.num_layers,
+                             args.dropout).to(device)
     else:
-        model = PyG_GCN(data.num_features, args.hidden_channels,
-                    dataset.num_classes, args.num_layers,
-                    args.dropout).to(device)
+        if args.catgnn:
+            print('--CATGNN GCN--')
+            model = GCN(data.num_features, args.hidden_channels,
+                        dataset.num_classes, args.num_layers,
+                        args.dropout).to(device)
+        else:
+            print('--PyG GCN--')
+            model = PyG_GCN(data.num_features, args.hidden_channels,
+                            dataset.num_classes, args.num_layers,
+                            args.dropout).to(device)
 
     evaluator = Evaluator(name='ogbn-arxiv')
     logger = Logger(args.runs, args)
@@ -288,8 +313,11 @@ def main():
         model.reset_parameters()
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
         for epoch in range(1, 1 + args.epochs):
-            loss = train(model, data, train_idx, optimizer)
-            result = test(model, data, split_idx, evaluator)
+            start = timer()
+            loss = train(model, data, train_idx, optimizer, not args.catgnn)
+            end = timer()
+
+            result = test(model, data, split_idx, evaluator, not args.catgnn)
             logger.add_result(run, result)
 
             if epoch % args.log_steps == 0:
@@ -299,7 +327,8 @@ def main():
                       f'Loss: {loss:.4f}, '
                       f'Train: {100 * train_acc:.2f}%, '
                       f'Valid: {100 * valid_acc:.2f}% '
-                      f'Test: {100 * test_acc:.2f}%')
+                      f'Test: {100 * test_acc:.2f}%'
+                      f'Runtime: {timedelta(seconds=end - start)}')
 
         logger.print_statistics(run)
     logger.print_statistics()
