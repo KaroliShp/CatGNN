76e8ea5d42b093e9b03a0ffbf8b78b711274ff4b
Karolishp <karolis.spukas@gmail.com>
2022-04-12 14:38:53 +0100

    #4 Refactor BaseMPNN classes

--- catgnn/integral_transform/mpnn_1.py
+++ catgnn/integral_transform/mpnn_1.py
@@ -1,26 +1,50 @@
+from typing import List
+
 import torch
 from torch import nn
+from torch import Tensor
 
 
 class BaseMPNNLayer_1(nn.Module):
     def __init__(self):
         super(BaseMPNNLayer_1, self).__init__()
 
-    """
-    Directed graph span construction
-    """
+    # Directed graph span construction
 
-    def _add_edge_indices(self, E):
-        self.E_indexed = torch.cat((E, torch.arange(0, E.shape[1]).view(1, -1)))
+    def _add_edge_indices(self, E: Tensor):
+        # Doesn't overwrite E - this have a memory cost, maybe TODO change?
+        self.E_indexed = torch.cat((E, torch.arange(0, E.shape[1], device=E.device).view(1, -1)))
 
-    def s(self, e):
+    def s(self, e: Tensor) -> Tensor:
+        """
+        s: E -> V
+        Get the sender node of an edge e
+
+        Args:
+            e (Tensor): an edge of shape (1,3), where e[0] is the sender,
+            e[1] is the receiver and e[2] is the index of the edge
+
+        Returns:
+            Tensor: sender node
+        """
         return self.E_indexed[0][e[2]]
 
-    def t(self, e):
+    def t(self, e: Tensor) -> Tensor:
+        """
+        t: E -> V
+        Get the receiver node of an edge e
+
+        Args:
+            e (Tensor): an edge of shape (1,3), where e[0] is the sender,
+            e[1] is the receiver and e[2] is the index of the edge
+
+        Returns:
+            Tensor: receiver node
+        """
         return self.E_indexed[1][e[2]]
 
-    def _set_preimages(self, V):
-        # This V always has to be full V (not chosen), since it is passed to self.transform()
+    def _set_preimages(self, V: Tensor):
+        # This V is always the full V (not chosen), since its called from within the class
         self._preimages = []
 
         # Create a list of lists, alternatively could be a dict
@@ -31,15 +55,33 @@ class BaseMPNNLayer_1(nn.Module):
         for i in range(0, self.E_indexed.shape[1]):
             self._preimages[self.E_indexed[1][i]].append(self.E_indexed[:, i])
 
-    def t_1(self, v):
+    def t_1(self, v: Tensor) -> List[Tensor]:
+        """
+        t^{-1}: V -> P(E)
+        Get preimages of node v.
+
+        Args:
+            v (Tensor): node
+
+        Returns:
+            List[Tensor]: list of edges belonging to the preimage of v
+        """
         return self._preimages[v]
 
-    def f(self, v):
+    def f(self, v: Tensor) -> Tensor:
+        """
+        f: V -> R
+        Function for features of nodes in V
+
+        Args:
+            v (Tensor): node
+
+        Returns:
+            Tensor: features
+        """        
         return self.X[v]
 
-    """
-    Integral transform primitives (backwards)
-    """
+    # Integral transform primitives (backwards)
 
     def define_pullback(self, f):
         def pullback(e):
@@ -68,16 +110,31 @@ class BaseMPNNLayer_1(nn.Module):
     def update(self, X, output):
         raise NotImplementedError
 
-    def transform_backwards(self, V, E, X, kernel_factor=False, validate_input=True):
+    def transform_backwards(self, V: Tensor, E: Tensor, X: Tensor, kernel_factor: bool = False, validate_input: bool = True) -> Tensor:
+        """
+        Integral transform implementation (backwards)
+
+        Args:
+            V (Tensor): set of nodes of shape (-1,) (each node from 0 to |V|-1)
+            E (Tensor): set of edges (sender to receiver) of shape (2,-1)
+            X (Tensor): set of features for each node in V of shape (|V|,num_features)
+            kernel_factor (bool, optional): whether to factorise the kernel arrow to have a 
+            receiver-dependent kernel. Defaults to False.
+            validate_input (bool, optional): whether to validate input before performing 
+            integral transform. Defaults to False.
+
+        Returns:
+            Tensor: set of updated features
+        """
         if validate_input:
             self._validate_input(V, E, X)
         
-        # Prepare edge indices for span diagram and the feature function f : V -> R
+        # Span construction
         self._add_edge_indices(E)
         self._set_preimages(V)
         self.X = X
 
-        # Prepare transform
+        # Prepare integral transform
         pullback = self.define_pullback(self.f)  # E -> R
         if kernel_factor:
             # Factoring kernel for MPNN_1 is not supported (no point because it is already slow)
@@ -87,25 +144,20 @@ class BaseMPNNLayer_1(nn.Module):
         pushforward = self.define_pushforward(kernel_transformation)  # V -> N[R]
         aggregator = self.define_aggregator(pushforward)  # V -> R
 
-        # Apply the transform to each node in the graph
-        updated_features = torch.Tensor()
+        # Apply the integral transform to each node in the graph, then finish with update step
+        updated_features = torch.Tensor(device=V.device)
         for v in V:
             updated_features = torch.hstack((updated_features, aggregator(v)))
-
         return self.update(X, updated_features.view(X.shape[0], -1))
 
-    """
-    Integral transform primitives (backwards)
-    """
+    # Integral transform primitives (forwards)
 
-    def transform_forwards(self, V, E, X, kernel_factor=False, validate_input=True):
+    def transform_forwards(self, V: Tensor, E: Tensor, X: Tensor, kernel_factor: bool = False, validate_input: bool = True) -> Tensor:
         # Forwards implementaton is not supported (no point because it is already slow)
         raise NotImplementedError
 
 
-    """
-    Other utils
-    """
+    # Other utils
 
     def _validate_input(self, V, E, X):
         # Firstly assert input types
--- catgnn/integral_transform/mpnn_2.py
+++ catgnn/integral_transform/mpnn_2.py
@@ -1,59 +1,112 @@
+from typing import Tuple
+
 import torch
 from torch import nn
+from torch import Tensor
 
 
 class BaseMPNNLayer_2(nn.Module):
     def __init__(self):
         super(BaseMPNNLayer_2, self).__init__()
 
-    """
-    Directed graph span construction
-    """
+    # Directed graph span constructions
 
-    def s(self, E):
+    def s(self, E: Tensor) -> Tensor:
+        """
+        s: E -> V
+        Get the sender of each node in set of edges E.
+
+        Args:
+            E (Tensor): set of edges (sender to receiver) of shape (2,-1)
+
+        Returns:
+            Tensor: senders of each edge
+        """
         return E[0]
 
-    def t(self, E):
+    def t(self, E: Tensor) -> Tensor:
+        """
+        t: E -> V
+        Get the receiver of each node in set of edges E.
+
+        Args:
+            E (Tensor): set of edges (sender to receiver) of shape (2,-1)
+
+        Returns:
+            Tensor: receivers of each edge
+        """
         return E[1]
 
-    def t_1(self, V):
-        # Get preimages of only those edges where the receiver is in V
-        # Return the edges in the preimage and the bag indices
-        # (which node's "bag"/preimage the edge belongs to)
-        # This chooses from all E, because which E to use can be selected by pullback later
+    def t_1(self, V: Tensor) -> Tuple[Tensor, Tensor]:
+        """
+        t^{-1}: V -> P(E)
+        Get preimages of nodes in V. This chooses from all E, because which E to use 
+        can be selected by pullback later.
+
+        Args:
+            V (Tensor): set of nodes of shape (-1,)
+
+        Returns:
+            Tuple[Tensor, Tensor]: the set of edges in all the preimages of V and
+            the bag indices (which node's "bag"/preimage the edge belongs to).
+        """
         selected_E = self.E.T[torch.isin(self.t(self.E), V)].T
         return selected_E, self.t(selected_E)
 
-    def get_opposite_edges(self, E, masking_required=False):
+    def get_opposite_edges(self, E: Tensor, masking_required: bool = False) -> Tensor:
+        """
+        *: E -> E
+        Get opposite edges of all edges in E as described in paper's appendix.
+
+        Args:
+            E (Tensor): set of edges (sender to receiver) of shape (2,-1)
+            masking_required (bool, optional): if True, then masking may be required if the 
+            opposite edge does not exist in the graph (e.x. for a directed graph, a -> b may 
+            exist, but b -> a may not exist).
+            Defaults to False.
+
+        Returns:
+            Tensor: set of opposite edges of the same shape as E
+        """
         # Flip row 0 and row 1 to create all the opposite edges
         flipped_E = torch.flip(E, [0])
 
         if masking_required:
-            # This is the case where masking is required, say in DP or in some GNN
-            # Now need to check that all such edges exist such that f assigns correct value when
-            # self.s(flipped_E) gives the sender. E.x. if the edge does not actually exist, it would be
-            # wrong to give the real value from f(self.s(flipped_E))
+            # Need to check that all opposite edges exist so that f assigns correct value when
+            # self.s(flipped_E) gives the sender
 
             # These values show that the edge at that index in flipped_E exists in E
             values, _ = torch.max(
                 (E == flipped_E.T.unsqueeze(-1)).all(dim=1).int(), dim=1
             )
-            inverse_values = (values - 1) * (-1)  # Inverse the mask (1 to 0 and 0 to 1)
-            return flipped_E.masked_fill(
-                inverse_values, -1
-            )  # Assign -1 to edges that don't exist
+
+            # Inverse the mask (1 to 0 and 0 to 1)
+            inverse_values = (values - 1) * (-1)
+
+            # Assign -1 to edges that don't exist. TODO: to make this work in 
+            # user implementation, we also need to append extra row to X which 
+            # contains the "default" value (e.x. torch.nan for Bellman-Ford)
+            return flipped_E.masked_fill(inverse_values, -1)
         else:
-            # No masking is required (for example in MPNN, when we pullback features from sender to the edge,
-            # we don't care if there is an edge from receiver back to sender). I guess if you care that would be a
-            # type of MPNN, not the general MPNN.
+            # No masking is required (for example in GCN or GAT when we pullback 
+            # features from sender to the edge, we don't care if there is an edge 
+            # from receiver back to sender)
             return flipped_E
 
-    def f(self, V):
+    def f(self, V: Tensor) -> Tensor:
+        """
+        f: V -> R
+        Function for features of nodes in V
+
+        Args:
+            V (Tensor): set of nodes of shape (-1,)
+
+        Returns:
+            Tensor: features
+        """
         return self.X[V]
 
-    """
-    Integral transform primitives (backwards)
-    """
+    # Integral transform primitives (backwards)
 
     def define_pullback(self, f):
         def pullback(E):
@@ -91,22 +144,44 @@ class BaseMPNNLayer_2(nn.Module):
 
         return aggregator
 
-    def update(self, X, output):
+    def update(self, X: Tensor, output: Tensor) -> Tensor:
         raise NotImplementedError
 
-    def transform_backwards(self, V, E, X, kernel_factor=False, validate_input=False):
+    def transform_backwards(
+        self,
+        V: Tensor,
+        E: Tensor,
+        X: Tensor,
+        kernel_factor: bool = False,
+        validate_input: bool = False,
+    ) -> Tensor:
+        """
+        Integral transform implementation (backwards)
+
+        Args:
+            V (Tensor): set of nodes of shape (-1,) (each node from 0 to |V|-1)
+            E (Tensor): set of edges (sender to receiver) of shape (2,-1)
+            X (Tensor): set of features for each node in V of shape (|V|,num_features)
+            kernel_factor (bool, optional): whether to factorise the kernel arrow to have a 
+            receiver-dependent kernel. Defaults to False.
+            validate_input (bool, optional): whether to validate input before performing 
+            integral transform. Defaults to False.
+
+        Returns:
+            Tensor: set of updated features
+        """
         if validate_input:
             self._validate_input(V, E, X)
 
-        # Set the span diagram (preimage) and feature function f : V -> R
+        # Save in the object for span constructions
         self.X = X
         self.E = E
 
-        # Prepare transform
+        # Prepare integral transform
         pullback = self.define_pullback(self.f)  # E -> R
         if kernel_factor:
-            # Hide extra edges as implementation detail
-            # This is not needed for MPNNs
+            # TODO: if masking is required, this would hide extra edges as implementation detail. 
+            # This is not needed for our use cases so far.
             # self.X = torch.cat((self.X, torch.ones(1,*self.X.shape[1:])*torch.inf),dim=0)
 
             product_arrow = self.define_kernel_factor_1(pullback)  # (E -> R) x (E -> R)
@@ -116,12 +191,10 @@ class BaseMPNNLayer_2(nn.Module):
         pushforward = self.define_pushforward(kernel_transformation)  # V -> N[R]
         aggregator = self.define_aggregator(pushforward)  # V -> R
 
-        # Apply the transform to each node in the graph, then finish with update step
+        # Apply the integral transform to each node in the graph, then finish with update step
         return self.update(X, aggregator(V))
 
-    """
-    Integral transform primitives (forwards)
-    """
+    # Integral transform primitives (forwards)
 
     def pullback(self, E, f):
         raise NotImplementedError
@@ -144,66 +217,83 @@ class BaseMPNNLayer_2(nn.Module):
     def update(self, X, output):
         raise NotImplementedError
 
-    def transform_forwards(self, V, E, X, kernel_factor=False, validate_input=True):
+    def transform_forwards(
+        self,
+        V: Tensor,
+        E: Tensor,
+        X: Tensor,
+        kernel_factor: bool = False,
+        validate_input: bool = False,
+    ) -> Tensor:
+        """
+        Integral transform implementation (forwards)
+
+        Args:
+            V (Tensor): set of nodes of shape (-1,) (each node from 0 to |V|-1)
+            E (Tensor): set of edges (sender to receiver) of shape (2,-1)
+            X (Tensor): set of features for each node in V of shape (|V|,num_features)
+            kernel_factor (bool, optional): whether to factorise the kernel arrow to have a 
+            receiver-dependent kernel. Defaults to False.
+            validate_input (bool, optional): whether to validate input before performing 
+            integral transform. Defaults to False.
+
+        Returns:
+            Tensor: set of updated features
+        """
         if validate_input:
             self._validate_input(V, E, X)
-        
-        # Set the span diagram (preimage) and feature function f : V -> R
+
+        # Save in the object for span constructions
         self.X = X
 
         # Pull node features along the span into edge features
         pulledback_features, chosen_E = self.pullback(E, self.f)
-        # print(f'pulledback features: {pulledback_features}\n')
 
         if kernel_factor:
+            # Not implemented at the moment (I don't particularly like forwards and I think 
+            # backwards implementation is better)
             raise NotImplementedError
         else:
-            # Do the kernel transformation on pulled node features
-            # We need selected_E to know which pulledback features belong to which edges
-            # These edge messages are only edge messages for the pulled back features
-            # In other words, assert edge_messages.shape[0] == pulledback_features.shape[0]
+            # Do the kernel transformation on pulled node features. We need chosen_E to know
+            # which pulled features belong to which edges, because we may choose edges
+            # arbitrarily. These edge messages are edge messages only for the pulled features
             edge_messages = self.kernel_transformation(chosen_E, pulledback_features)
-            # print(f'edge messages: {edge_messages}\n')
 
-        # For each receiver, go over their preimage edges and collect the edge messages into bags
+        # For each receiver, go over its preimage edges and collect the edge messages into bags
         # We can select which receivers we want here? Or later for aggregator? TODO
         # Note that pushforward will get preimages for V from all edges
         # However, it should only get preimages out of edges that were selected by pullback
         self.E = chosen_E
         edge_messages, bag_indices = self.pushforward(V, edge_messages)
-        # print(f'edge messages: {edge_messages}, bag indices: {bag_indices}\n')
 
         # Aggregate for selected V
         aggregated_output = self.aggregator(V, edge_messages, bag_indices)
-        # print(f'aggregated output: {aggregated_output}\n')
 
         # Update and return
         return self.update(X, aggregated_output)
 
-    """
-    Other utils
-    """
+    # Other utils
 
-    def _validate_input(self, V, E, X):
+    def _validate_input(self, V: Tensor, E: Tensor, X: Tensor):
         # Firstly assert input types
-        assert type(V) == torch.Tensor, f'V must be a torch.Tensor, not {type(V)}'
-        assert type(E) == torch.Tensor, f'E must be a torch.Tensor, not {type(E)}'
-        assert type(X) == torch.Tensor, f'X must be a torch.Tensor, not {type(X)}'
+        assert type(V) == torch.Tensor, f"V must be a torch.Tensor, not {type(V)}"
+        assert type(E) == torch.Tensor, f"E must be a torch.Tensor, not {type(E)}"
+        assert type(X) == torch.Tensor, f"X must be a torch.Tensor, not {type(X)}"
 
         # Then assert tensor properties
-        assert V.dtype == torch.int64, f'V.dtype must be torch.int64, not {V.dtype}'
-        assert len(V.shape) == 1, f'V must be a 1D tensor'
-        assert V.shape[0] != 0, f'V cannot be empty'
+        assert V.dtype == torch.int64, f"V.dtype must be torch.int64, not {V.dtype}"
+        assert len(V.shape) == 1, f"V must be a 1D tensor"
+        assert V.shape[0] != 0, f"V cannot be empty"
 
-        assert E.dtype == torch.int64, f'E.dtype must be torch.int64, not {E.dtype}'
-        assert len(E.shape) == 2, f'V must be a 2D tensor'
-        assert E.shape[0] == 2, f'E must have two rows/must have shape of (2,-1)'
-        assert E.shape[1] != 0, f'E cannot be empty'
+        assert E.dtype == torch.int64, f"E.dtype must be torch.int64, not {E.dtype}"
+        assert len(E.shape) == 2, f"V must be a 2D tensor"
+        assert E.shape[0] == 2, f"E must have two rows/must have shape of (2,-1)"
+        assert E.shape[1] != 0, f"E cannot be empty"
 
-        assert X.shape[0] != 0, f'X cannot be empty'
+        assert X.shape[0] != 0, f"X cannot be empty"
 
         # Now assert that the shapes of V, E and X together make sense
-        assert V.shape[0] == X.shape[0], f'V must have the same shape as X'
+        assert V.shape[0] == X.shape[0], f"V must have the same shape as X"
 
         # TODO: maybe also assert devices (easier to debug)?
         # TODO: maybe also create a new method for checking that the implementation is
